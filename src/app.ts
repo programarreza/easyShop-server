a31057f40f9430b622e0e0b1b64c7666e967c15c
programarreza <programarreza@gmail.com>
2024-12-02 23:54:32 +0600

    initial setup

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Application, NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import router from "./app/routers";
+
+const app: Application = express();
+app.use(cors());
+app.use(cookieParser());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+// app.use("/api/v1", router);
+
+app.use(globalErrorHandler);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(StatusCodes.NOT_FOUND).json({
+    success: false,
+    message: "API NOT FOUND!",
+    error: {
+      path: req.originalUrl,
+      method: req.method,
+      message: "Your request path is not found!",
+    },
+  });
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "easyShop server",
+  });
+});
+
+export default app;
